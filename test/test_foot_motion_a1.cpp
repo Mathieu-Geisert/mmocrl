a40325cffd9d6f18c8dcc9b43aecc80e2cbd3112
Mathieu Geisert <mathieu@robots.ox.ac.uk>
2021-09-06 16:28:57 +0100

    Test foot motion.

--- test/test_foot_motion_a1.cpp
+++ test/test_foot_motion_a1.cpp
@@ -18,8 +18,8 @@ using namespace std::chrono;
 
 int main(int argc, char *argv[]) {
 
-  std::string urdf_path = "/home/pshah/Applications/mmocrl/rsc/robot/a1/urdf/a1.urdf";
-  std::string actuator_network_path = "/home/pshah/Applications/mmocrl/rsc/actuator/C100/seaModel_2500.txt";
+  std::string urdf_path = "/home/mgeisert/raisim_ws/src/mmocrl/rsc/robot/a1/urdf/a1.urdf";
+  std::string actuator_network_path = "/home/mgeisert/raisim_ws/src/mmocrl/rsc/actuator/C100/seaModel_2500.txt";
   
   raisim::World* world = new raisim::World();
   raisim::RaisimServer* server = new raisim::RaisimServer(world);
@@ -42,12 +42,13 @@ int main(int argc, char *argv[]) {
   gc_init.tail(12) = a1Params.getReferenceJointConfiguration();
   a1->setState(gc_init.template cast<double>(), gv_init.template cast<double>());
 
-  NoisyState robotState(a1, &rn);
-  FootMotionGenerator footMotion(&a1Params, &robotState, 2, 0.2, 0.0025);
+  State<float> robotState(a1);
+  FootMotionGenerator footMotion(&a1Params, &robotState, 3.0, 0.0, 0.0025);
   Eigen::Vector3f e_g, sol;
   e_g.setZero();
   e_g[2] = 1.;
-   
+
+  std::cout << "reference offset: " << a1Params.getReferenceFootPositionOffset().transpose() << std::endl; 
   Eigen::Matrix<float, 12, 1> gc_target, foot_target;
   Eigen::Vector4f deltaFreq;
   deltaFreq.setZero();
@@ -59,6 +60,7 @@ int main(int argc, char *argv[]) {
     auto start = high_resolution_clock::now();
     robotState.updateState();
     foot_target = footMotion.advance(deltaFreq);
+    std::cout << footMotion.getPhases()[0] << " -- " << foot_target.transpose() << std::endl;
     auto stop = high_resolution_clock::now();
     for (int j = 0; j < 4; j++) {
       IK.IKSagittal(sol, foot_target.segment(3 * j, 3), j);
@@ -68,7 +70,15 @@ int main(int argc, char *argv[]) {
       }
       gc_target.segment(3 * j, 3) = sol;
     }
+    gc_init[2] = 0.45;
     gc_init.tail(12) = gc_target;
+    Eigen::Quaterniond q;
+    q = Eigen::AngleAxisd(std::sin(footMotion.getPhases()[0])*0.6, Eigen::Vector3d::UnitX());
+    //q = Eigen::AngleAxisd(std::sin(0)*0.6, Eigen::Vector3d::UnitX());
+    gc_init[3] = q.w();
+    gc_init[4] = q.x();
+    gc_init[5] = q.y();
+    gc_init[6] = q.z();
     //actuator.setTargetJointPosition(gc_target);
     //actuator.advance();
     a1->setState(gc_init.template cast<double>(), gv_init.template cast<double>());
@@ -77,7 +87,7 @@ int main(int argc, char *argv[]) {
     server->lockVisualizationServerMutex();
     world->integrate();
     server->unlockVisualizationServerMutex();
-    usleep(2500);
+    usleep(2500*4);
     if (i%400 == 0)
       std::cout << "s=" << i/400 << std::endl;
   }
